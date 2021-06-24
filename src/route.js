548270375bfc730053e333bce07bfb4bb904c691
Thallysson Silva <sthallysson5@gmail.com>
2021-06-24 18:00:09 -0300

    install express/ejs , mudando html pra ejs e algumas alterações no node

---
+++ src/route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const route = express.Router();
+
+route.get("/", (req, res) => res.render("index"));
+route.get("/room-empty", (req, res) => res.render("room-empty"));
+route.get("/room", (req, res) => res.render("room"));
+route.get("/create-pass", (req, res) => res.render("create-pass"));
+
+//formato que o formulario de dentro da modal tem que passar imformação
+//route.post("/room/:room/:question/:action");
+
+module.exports = route;
