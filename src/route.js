c4cfd0a6f4511b58f862600f9cb41f930d44e3c3
Thallysson Silva <sthallysson5@gmail.com>
2021-06-24 23:50:25 -0300

    link pra criar sala

--- src/route.js
+++ src/route.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
+const RoomController = require("./controllers/RoomController");
 
 const route = express.Router();
 
-route.get("/", (req, res) => res.render("index"));
-route.get("/room-empty", (req, res) => res.render("room-empty"));
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-pass", (req, res) =>
+  res.render("index", { page: "create-pass" })
+);
+
+route.get("/room-empty/:room", (req, res) => res.render("room-empty"));
 route.get("/room", (req, res) => res.render("room"));
-route.get("/create-pass", (req, res) => res.render("create-pass"));
 
 //formato que o formulario de dentro da modal tem que passar imformação
-route.post("/room/:room/:question/:action", QuestionController.index);
+route.post("/question/:room/:question/:action", QuestionController.index);
+route.post("/create-room", RoomController.create);
 
 module.exports = route;
