1a93ad740a5103da3b6b7c27c343f86dc986cc2c
Thallysson Silva <sthallysson5@gmail.com>
2021-06-26 18:07:13 -0300

    ajustes finais

--- src/route.js
+++ src/route.js
@@ -9,11 +9,11 @@ route.get("/create-pass", (req, res) =>
   res.render("index", { page: "create-pass" })
 );
 
-route.get("/room-empty/:room", (req, res) => res.render("room-empty"));
-route.get("/room", (req, res) => res.render("room"));
-
-//formato que o formulario de dentro da modal tem que passar imformação
-route.post("/question/:room/:question/:action", QuestionController.index);
 route.post("/create-room", RoomController.create);
+route.get("/room/:room", RoomController.open);
+route.post("/enterroom", RoomController.enter);
+
+route.post("/question/create/:room", QuestionController.create);
+route.post("/question/:room/:question/:action", QuestionController.index);
 
 module.exports = route;
